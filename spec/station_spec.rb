63f3e2493044da3f4594eb4dfad2c87b6ab8b3ba
Daniel Tooke <daniel.tooke@gmail.com>
2018-03-28 15:47:49 +0100

    Added Station class and initial tests

---
+++ spec/station_spec.rb
@@ -0,0 +1,15 @@
+require 'station'
+
+describe Station do
+
+let (:subject) {Station.new("Kings Cross", 1)}
+
+  it 'should have an exposed name instance variable' do
+    expect(subject).to respond_to(:name)
+  end
+
+  it 'should have an exposed zone instance variable' do
+    expect(subject).to respond_to(:name)
+  end
+
+end
